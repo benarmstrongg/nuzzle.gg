809660b7f3a4bc2970ce65638d6b4463d3233dcb
ben <benarmstrongg@gmail.com>
2024-06-30 14:09:14 -0400

    Fix ItemIcon implementation.

--- src/objects/item-icon.obj.ts
+++ src/objects/item-icon.obj.ts
@@ -1,25 +1,17 @@
-import { Assets, Container, Texture, TextureSource } from 'pixi.js';
-import { SpriteObject } from '../engine';
+import { Assets, Texture, TextureSource } from 'pixi.js';
+import { SpriteObject, ContainerObject } from '../engine';
 import { toID } from '../../../pokemon-showdown/sim/dex-data';
 
 export class ItemIcon extends SpriteObject {
-    async setItem(item: string, container: Container) {
+    async setItem(item: string, container: ContainerObject) {
         const id = toID(item);
-        let url: string;
-        if (id.includes('berry')) {
-            url = `assets/sprites/items/berries/${id}.png`;
-            this.scale = 0.5;
-        } else {
-            url = `assets/sprites/items/${id}.png`;
-            this.scale = 0.8;
-        }
+        const url = `assets/sprites/items/${id}.png`;
         let asset: TextureSource;
         try {
             asset = await Assets.load(url);
         } catch {
             asset = await Assets.load('assets/sprites/items/xspatk.png');
         }
-        this.label = 'hello';
         this.setTexture(new Texture(asset), container);
     }
 }
