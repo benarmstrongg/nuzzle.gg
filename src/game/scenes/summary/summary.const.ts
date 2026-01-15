9f69edecc634df21fb8b657f875d48e170231251
ben <benarmstrongg@gmail.com>
2026-01-14 21:43:19 -0500

    Delete legacy view

--- src/game/scenes/summary/summary.const.ts
+++
@@ -1,9 +0,0 @@
-export const ASSETS = {
-    BG: [
-        'sprites/ui/summary/bg_info.png',
-        'sprites/ui/summary/bg_notes.png',
-        'sprites/ui/summary/bg_stats.png',
-        'sprites/ui/summary/bg_moves.png',
-        'sprites/ui/summary/bg_ribbons.png',
-    ],
-} as const;
